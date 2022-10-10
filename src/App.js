46f5ad0acf85e7c80c16a5ec4085177c638d5903
lr-colosimo <46933429+lr-colosimo@users.noreply.github.com>
2022-10-09 18:29:55 -0700

    Setup skeleton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar
+} from '@mui/material';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<Layout/>}>
+              <Route index element={<Home/>} />
+              <Route path="custom" element={<h2>Custom</h2>} />
+            </Route>
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
+}
+
+function Layout() {
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>Title</Toolbar>
+      </AppBar>
+      <Outlet/>
+    </div>
+  )
+}
+
+function Home() {
+  return (
+    <h1>Home</h1>
+  )
 }
 
 export default App;
